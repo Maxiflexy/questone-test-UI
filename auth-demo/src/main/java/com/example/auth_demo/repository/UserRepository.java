4f02c9854dcc370fe4b5e6b1dfb1a5af5de64442
Maxiflexy <onyemax247@gmail.com>
2025-07-23 19:17:11 +0100

    added reg

--- auth-demo/src/main/java/com/example/auth_demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth_demo.repository;
-
-import com.example.auth_demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
